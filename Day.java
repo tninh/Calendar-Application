d92703df8da6cea99fb35e1f994300bd77e619ea
Doppeltongue <wasdqwe@gmail.com>
2015-11-24 19:35:19 -0800

    Update Day.java

--- Day.java
+++ Day.java
@@ -10,21 +10,34 @@ import java.util.ArrayList;
 
 public class Day {
 	private ArrayList<Event> eventlist;
-	
+	public String label;
 	
 	public Day() {
 		eventlist = new ArrayList<Event>();
+		label = "";
 	}
 	
 	public void add(Event e) {
 		eventlist.add(e);
 	}
 	
-	public boolean isEmpty() {
+	public boolean hasEvent() {
 		return eventlist.isEmpty();
 	}
 	
 	public void remove(Event e) {
 		eventlist.remove(e);
+	}
+	
+	public void setLabel(String s) {
+		label = s;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public ArrayList<Event> getEvents() {
+		return eventlist;
 	}
 }
